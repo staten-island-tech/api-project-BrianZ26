83f2a5747fec7ffe5873112e112eef9c2f4901b4
BrianZ26 <brian.zhao026@gmail.com>
2023-01-11 13:29:05 -0500

    a

---
+++ API-project/main.js
@@ -0,0 +1,18 @@
+const pokemon = "ditto";
+const pokeinfo = `https://pokeapi.co/api/v2/pokemon/${pokemon}`;
+async function getData(pokeinfo) {
+  try {
+    const response = await fetch(pokeinfo);
+    if (response.status < 200 || response.status > 299) {
+      console.log(response.status);
+      throw error(response);
+    } else {
+      const data = await response.json();
+      console.log(data);
+    }
+  } catch (error) {
+    console.log(error);
+    console.log("sad");
+  }
+}
+getData(ditto);
